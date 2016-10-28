2be7b82654706729d7717a3b88e3f142b09e6e0e
Jeff Larson <thejefflarson@gmail.com>
2016-10-27 21:21:02 -0400

    labels for function

--- src/visitor.hh
+++ src/visitor.hh
@@ -12,6 +12,7 @@ class Visitor {
   virtual void visit(const Identifier &identifier) = 0;
   virtual void visit(const String &string) = 0;
   virtual void visit(const Program &program) = 0;
+  virtual void visit(const Argument &function) = 0;
   virtual void visit(const Function &function) = 0;
   virtual void visit(const Application &application) = 0;
   virtual void visit(const Conditional &conditional) = 0;
