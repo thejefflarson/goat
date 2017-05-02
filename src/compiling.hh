6146ef7a57b84a3c70dd9b7976f5f706ad5cce04
Jeff Larson <thejefflarson@gmail.com>
2017-05-02 18:05:13 -0400

    llvm structures in CompilingVisitor

--- src/compiling.hh
+++ src/compiling.hh
@@ -1,7 +1,9 @@
 #ifndef SRC_COMPILING_
 #define SRC_COMPILING_
 
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 
 #include "inference.hh"
 #include "node.hh"
@@ -15,12 +17,17 @@ class CompilingVisitor : public node::Visitor {
 public:
   CompilingVisitor(std::set<inference::Substitution> substitutions) :
     substitutions_(substitutions),
+    context_(),
+    builder_(context_),
+    module_(std::move(llvm::make_unique<llvm::Module>("Goat Compiler", context_))),
     scope_() {}
   VisitorMethods
 private:
   std::set<inference::Substitution> substitutions_;
-  std::set<node::Identifier> scope_;
   llvm::LLVMContext context_;
+  llvm::IRBuilder<> builder_;
+  std::unique_ptr<llvm::Module> module_;
+  std::map<std::string, llvm::Value *> scope_;
 };
 
 }
